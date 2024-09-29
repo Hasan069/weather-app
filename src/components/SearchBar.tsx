aff09dbbd58015c80c48817a6f2b8cf6c8b6c4ce
Cyanider <amihasan420@gmail.com>
2024-09-30 01:13:48 +0600

    Day 7

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+"use client"
+
+
+import React,{useState} from "react";
 import { Input } from "@/components/ui/input";
 import { TbCloudSearch } from "react-icons/tb";
 
 const SearchBar = () => {
+  const [city, setCity] = useState<string>(''); // State to store the city name input
+  
+  
   return (
+
+    
     <div>
       <div className="flex items-center gap-4 -translate-y-10">
         <Input
           type="city"
           placeholder="Search for your city"
           className="h-12 text-lg rounded-2xl"
+          value={city}
+          onChange={(e) => setCity(e.target.value)} // Update city state as user types
+          
         />
         <TbCloudSearch className="h-10 w-10 cursor-pointer" />
       </div>
--- src/components/WeatherBody.tsx
+++ src/components/WeatherBody.tsx
@@ -4,11 +4,24 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import cloud from "@/assets/cloud.png";
 import { IoWaterOutline } from "react-icons/io5";
+import { FaWind } from "react-icons/fa";
+import { GiBottleVapors } from "react-icons/gi";
+import { FaTemperatureArrowDown, FaTemperatureArrowUp } from "react-icons/fa6";
+import { FaCloudRain } from "react-icons/fa";
+import { MdOutlineVisibility } from "react-icons/md";
+import { FaCloud } from "react-icons/fa";
 
 const WeatherBody = () => {
   const [weatherData, setWeatherData] = useState<any>("");
+  const [sunrise, setSunrise] = useState<string | null>(null);
+  const [sunset, setSunset] = useState<string | null>(null);
   const API_KEY = "967f5832c40b86b12c421754abf2608a";
   const city = "Dhaka";
+
+  const convertUnixToTime = (timestamp: number): string => {
+    const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
+    return date.toLocaleTimeString(); // Returns the time in a readable format (HH:MM:SS AM/PM)
+  };
 
   useEffect(() => {
     const fetchWeatherData = async () => {
@@ -18,6 +31,9 @@ const WeatherBody = () => {
         );
         const data = await response.json();
         setWeatherData(data);
+
+        setSunrise(convertUnixToTime(data.sys.sunrise));
+        setSunset(convertUnixToTime(data.sys.sunset));
       } catch (error) {
         console.log("error fetcing weather data", error);
       }
@@ -26,43 +42,118 @@ const WeatherBody = () => {
   }, []);
 
   return (
-    <>
-      <div className="-translate-y-7">
-        <div className="container w-[50vw] md:min-w-[450px] mx-auto flex justify-center items-center">
-          <Image src={cloud} alt="cloud" className="h-40 w-52"></Image>
-        </div>
-        <div className="text-center">
-          <p className="font-bold text-5xl">{weatherData.main?.temp}C°</p>
-          <p className="font-semiboldbold text-2xl">cloudy</p>
-        </div>
-
-        <div className="weather-details mt-3 flex justify-around">
-          <div className="flex gap-2">
-            <IoWaterOutline className="h-16 w-16" />
-            <div className="mt-1">
-              <p>Humidity: </p>
-              <span className="text-xl">{weatherData.main?.humidity}%</span>
-            </div>
+    <div className="-translate-y-7">
+      {weatherData ? (
+        <div>
+          <div className="container w-[50vw] md:min-w-[450px] mx-auto flex justify-center items-center">
+            <Image src={cloud} alt="cloud" className="h-40 w-52"></Image>
+          </div>
+          <div className="text-center ">
+            
+            <p className="font-bold text-4xl mt-4">
+              {weatherData.main?.temp}C° {/* this line shows temparature*/}
+            </p>
+            <p className="font-bold text-xl mt-5">
+              feels like {weatherData.main?.feels_like}C°
+            </p>
           </div>
 
-          <div className="flex gap-2">
-            <IoWaterOutline className="h-16 w-16" />
-            <div className="mt-1">
-              <span className="text-xl">{weatherData.wind?.speed} m</span>
-              <p>Wind Speed</p>
+          <div className="weather-details flex justify-around overflow-x-scroll mt-10 gap-1 translate-y-16">
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <IoWaterOutline className="h-16 w-16" />
+              <div>
+                <p>Humidity</p>
+                <span>{weatherData.main?.humidity}%</span>{" "}
+                {/* this line shows humidity*/}
+              </div>
             </div>
-          </div>
 
-          <div className="flex gap-2">
-            <IoWaterOutline className="h-16 w-16" />
-            <div className="mt-1">
-              <span className="text-xl"> Precipitation:</span>
-              <p> {weatherData.weather?.[0]?.description} </p>
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <FaWind className="h-16 w-16" />
+              <div>
+                <p>Wind Speed</p>
+                <span> {weatherData.wind?.speed}m</span>{" "}
+                {/* this line shows wind speed*/}
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <GiBottleVapors className="h-16 w-16" />
+              <div>
+                <p>Precipitation</p>
+                <span> {weatherData.weather?.[0]?.description}</span>{" "}
+                {/* this line shows Precipitation*/}
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <FaTemperatureArrowDown className="h-16 w-16" />
+              <div>
+                <span>Minimum Temparature</span>
+                <p> {weatherData.main?.temp_min} </p>{" "}
+                {/* this line shows minimum Temparature*/}
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <FaTemperatureArrowUp className="h-16 w-16" />
+              <div>
+                <span>Maximum Temparature</span>
+                <p> {weatherData.main?.temp_max} </p>{" "}
+                {/* this line shows maximum temparature*/}
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <FaCloudRain className="h-16 w-16" />
+              <div>
+                <span>Rain</span> {/* this line shows will it rain or not*/}
+                <p>
+                  {weatherData.rain?.["1h"]
+                    ? `${weatherData.rain?.["1h"]} mm in the last 1 hour`
+                    : "No rain data available"}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <FaCloud  className="h-16 w-16" />
+              <div>
+                <span>cloud</span> {/* this line shows cloud*/}
+                <p >{weatherData.clouds?.all}</p>
+              </div>
+            </div>
+
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <MdOutlineVisibility className="h-16 w-16" />
+              <div>
+                <span>Visibility</span>
+                <p> {weatherData.visibility} </p>
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <MdOutlineVisibility className="h-16 w-16" />
+              <div>
+                <span>sunrise</span>
+                <p> {sunrise} </p>
+              </div>
+            </div>
+
+            <div className="flex p-3 h-24 w-52  border border-slate-700 rounded-xl gap-2">
+              <MdOutlineVisibility className="h-16 w-16" />
+              <div>
+                <span>sunset</span>
+                <p> {sunset} </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      ) : (
+        <p>Loading weather data....</p>
+      )}
+    </div>
   );
 };
 
