01b20ef2170e56fded0cbdd1c137db317da0eda9
Cyanider <amihasan420@gmail.com>
2024-09-29 18:04:47 +0600

    Day 5

--- src/components/WeatherBody.tsx
+++ src/components/WeatherBody.tsx
@@ -1,12 +1,14 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import cloud from "@/assets/cloud.png";
 import { IoWaterOutline } from "react-icons/io5";
 
 const WeatherBody = () => {
-  const [weatherData, setWeatherData] = useState(null);
-  const API_KEY = "";
-  const city = "";
+  const [weatherData, setWeatherData] = useState<any>("");
+  const API_KEY = "967f5832c40b86b12c421754abf2608a";
+  const city = "Dhaka";
 
   useEffect(() => {
     const fetchWeatherData = async () => {
@@ -24,41 +26,43 @@ const WeatherBody = () => {
   }, []);
 
   return (
-    <div className="-translate-y-7">
-      <div className="container w-[50vw] md:min-w-[450px] mx-auto flex justify-center items-center">
-        <Image src={cloud} alt="cloud" className="h-40 w-52"></Image>
-      </div>
-      <div className="text-center">
-        <p className="font-bold text-5xl">24 C°</p>
-        <p className="font-semiboldbold text-2xl">cloudy</p>
-      </div>
-
-      <div className="weather-details mt-3 flex justify-around">
-        <div className="flex gap-2">
-          <IoWaterOutline className="h-16 w-16" />
-          <div className="mt-1">
-            <span className="text-xl">0%</span>
-            <p>Humidity</p>
-          </div>
+    <>
+      <div className="-translate-y-7">
+        <div className="container w-[50vw] md:min-w-[450px] mx-auto flex justify-center items-center">
+          <Image src={cloud} alt="cloud" className="h-40 w-52"></Image>
+        </div>
+        <div className="text-center">
+          <p className="font-bold text-5xl">{weatherData.main?.temp}C°</p>
+          <p className="font-semiboldbold text-2xl">cloudy</p>
         </div>
 
-        <div className="flex gap-2">
-          <IoWaterOutline className="h-16 w-16" />
-          <div className="mt-1">
-            <span className="text-xl">0%</span>
-            <p>Humidity</p>
+        <div className="weather-details mt-3 flex justify-around">
+          <div className="flex gap-2">
+            <IoWaterOutline className="h-16 w-16" />
+            <div className="mt-1">
+              <p>Humidity: </p>
+              <span className="text-xl">{weatherData.main?.humidity}%</span>
+            </div>
           </div>
-        </div>
 
-        <div className="flex gap-2">
-          <IoWaterOutline className="h-16 w-16" />
-          <div className="mt-1">
-            <span className="text-xl">0%</span>
-            <p>Humidity</p>
+          <div className="flex gap-2">
+            <IoWaterOutline className="h-16 w-16" />
+            <div className="mt-1">
+              <span className="text-xl">{weatherData.wind?.speed} m</span>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+
+          <div className="flex gap-2">
+            <IoWaterOutline className="h-16 w-16" />
+            <div className="mt-1">
+              <span className="text-xl"> Precipitation:</span>
+              <p> {weatherData.weather?.[0]?.description} </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
